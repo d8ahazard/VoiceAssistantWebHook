5a38b4ff881cc1c624f092fde1d58140f6066c9c
d8ahazard <d8ahazard@gmail.com>
2017-01-24 11:41:45 -0600

    Try reading an API Key

--- assistanthandlers/phlex/__init__.py
+++ assistanthandlers/phlex/__init__.py
@@ -4,7 +4,11 @@ class AssistantHandlerPhlex(AssistantHandler):
 	def __init__(self):
 		AssistantHandler.__init__(self,"Playmedia")
 
-
+	def getApiToken(self,parameters, data):
+		query = data.get('apiToken')
+        if query is None:
+            return "No API Key specified"
+		
 	def getBaseUrl(self,parameters):
 		pass
 
@@ -17,4 +21,4 @@ class AssistantHandlerPhlex(AssistantHandler):
 		pass
 
 	def getSpeech(self, parameters, data):
-		return "Phlex is listening!"
+		return "Phlex is listening!  And I have your api key" + query
