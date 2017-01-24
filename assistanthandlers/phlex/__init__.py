1f7c6fab7dbaef6387af6eadee3ac534eb1048c5
d8ahazard <d8ahazard@gmail.com>
2017-01-24 11:45:28 -0600

    Try again

--- assistanthandlers/phlex/__init__.py
+++ assistanthandlers/phlex/__init__.py
@@ -4,21 +4,14 @@ class AssistantHandlerPhlex(AssistantHandler):
 	def __init__(self):
 		AssistantHandler.__init__(self,"Playmedia")
 
-	def getApiToken(self,parameters, data):
-		query = data.get('apiToken')
+	def getSpeech(self, parameters, data):
+        query = data.get('query')
         if query is None:
-            return "No API Key specified"
-		
-	def getBaseUrl(self,parameters):
-		pass
+            return "No query"
 
-
-	def getEndpoint(self,parameters):
-		pass
-
-
-	def getEndpointParameters(self,parameters):
-		pass
+        result = query.get('apiToken')
+        if result is None:
+            return "No results"
 
 	def getSpeech(self, parameters, data):
-		return "Phlex is listening!  And I have your api key" + query
+		return "Phlex is listening!  And I have your api key" + result
